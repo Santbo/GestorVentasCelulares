b7e92e860fdd0a56a1c0a534b8a9fe60a84aa471
maxacan <maximilianoacan@gmail.com>
2025-09-20 15:01:38 -0300

    UI Detalle compra temrinado

--- GestionVentasCel/views/compra/VerDetallesCompraForm.Designer.cs
+++ GestionVentasCel/views/compra/VerDetallesCompraForm.Designer.cs
@@ -28,6 +28,8 @@ namespace GestionVentasCel.views.compra
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VerDetallesCompraForm));
             lblProveedor = new Label();
             lblFecha = new Label();
             lblTotal = new Label();
@@ -35,6 +37,9 @@ namespace GestionVentasCel.views.compra
             dgvDetalles = new DataGridView();
             btnCerrar = new Button();
             btnEditar = new Button();
+            btnSalir = new Button();
+            imageList1 = new ImageList(components);
+            lblTituloForm = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvDetalles).BeginInit();
             SuspendLayout();
             // 
@@ -42,41 +47,45 @@ namespace GestionVentasCel.views.compra
             // 
             lblProveedor.AutoSize = true;
             lblProveedor.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            lblProveedor.Location = new Point(14, 20);
+            lblProveedor.Location = new Point(18, 89);
+            lblProveedor.Margin = new Padding(4, 0, 4, 0);
             lblProveedor.Name = "lblProveedor";
-            lblProveedor.Size = new Size(98, 23);
+            lblProveedor.Size = new Size(121, 28);
             lblProveedor.TabIndex = 0;
-            lblProveedor.Text = "Proveedor:";
+            lblProveedor.Text = "Proveedor: ";
             // 
             // lblFecha
             // 
             lblFecha.AutoSize = true;
             lblFecha.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            lblFecha.Location = new Point(14, 59);
+            lblFecha.Location = new Point(18, 138);
+            lblFecha.Margin = new Padding(4, 0, 4, 0);
             lblFecha.Name = "lblFecha";
-            lblFecha.Size = new Size(60, 23);
+            lblFecha.Size = new Size(77, 28);
             lblFecha.TabIndex = 1;
-            lblFecha.Text = "Fecha:";
+            lblFecha.Text = "Fecha: ";
             // 
             // lblTotal
             // 
             lblTotal.AutoSize = true;
             lblTotal.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            lblTotal.Location = new Point(457, 20);
+            lblTotal.Location = new Point(571, 89);
+            lblTotal.Margin = new Padding(4, 0, 4, 0);
             lblTotal.Name = "lblTotal";
-            lblTotal.Size = new Size(64, 28);
+            lblTotal.Size = new Size(84, 32);
             lblTotal.TabIndex = 2;
-            lblTotal.Text = "Total:";
+            lblTotal.Text = "Total: ";
             // 
             // lblObservaciones
             // 
             lblObservaciones.AutoSize = true;
             lblObservaciones.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            lblObservaciones.Location = new Point(14, 97);
+            lblObservaciones.Location = new Point(18, 185);
+            lblObservaciones.Margin = new Padding(4, 0, 4, 0);
             lblObservaciones.Name = "lblObservaciones";
-            lblObservaciones.Size = new Size(130, 23);
+            lblObservaciones.Size = new Size(161, 28);
             lblObservaciones.TabIndex = 3;
-            lblObservaciones.Text = "Observaciones:";
+            lblObservaciones.Text = "Observaciones: ";
             // 
             // dgvDetalles
             // 
@@ -84,22 +93,22 @@ namespace GestionVentasCel.views.compra
             dgvDetalles.AllowUserToDeleteRows = false;
             dgvDetalles.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvDetalles.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvDetalles.Location = new Point(14, 140);
-            dgvDetalles.Margin = new Padding(3, 4, 3, 4);
+            dgvDetalles.Location = new Point(18, 240);
+            dgvDetalles.Margin = new Padding(4, 5, 4, 5);
             dgvDetalles.Name = "dgvDetalles";
             dgvDetalles.ReadOnly = true;
             dgvDetalles.RowHeadersWidth = 51;
             dgvDetalles.RowTemplate.Height = 25;
             dgvDetalles.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvDetalles.Size = new Size(686, 400);
+            dgvDetalles.Size = new Size(858, 500);
             dgvDetalles.TabIndex = 4;
             // 
             // btnCerrar
             // 
-            btnCerrar.Location = new Point(614, 560);
-            btnCerrar.Margin = new Padding(3, 4, 3, 4);
+            btnCerrar.Location = new Point(768, 753);
+            btnCerrar.Margin = new Padding(4, 5, 4, 5);
             btnCerrar.Name = "btnCerrar";
-            btnCerrar.Size = new Size(86, 40);
+            btnCerrar.Size = new Size(108, 50);
             btnCerrar.TabIndex = 5;
             btnCerrar.Text = "Cerrar";
             btnCerrar.UseVisualStyleBackColor = true;
@@ -107,20 +116,60 @@ namespace GestionVentasCel.views.compra
             // 
             // btnEditar
             // 
-            btnEditar.Location = new Point(521, 560);
-            btnEditar.Margin = new Padding(3, 4, 3, 4);
+            btnEditar.Location = new Point(651, 753);
+            btnEditar.Margin = new Padding(4, 5, 4, 5);
             btnEditar.Name = "btnEditar";
-            btnEditar.Size = new Size(86, 40);
+            btnEditar.Size = new Size(108, 50);
             btnEditar.TabIndex = 6;
             btnEditar.Text = "Editar";
             btnEditar.UseVisualStyleBackColor = true;
             btnEditar.Click += btnEditar_Click;
             // 
+            // btnSalir
+            // 
+            btnSalir.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnSalir.Cursor = Cursors.Hand;
+            btnSalir.FlatAppearance.BorderSize = 0;
+            btnSalir.FlatStyle = FlatStyle.Flat;
+            btnSalir.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSalir.ImageKey = "xmark-solid-full.png";
+            btnSalir.ImageList = imageList1;
+            btnSalir.Location = new Point(844, 12);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(32, 32);
+            btnSalir.TabIndex = 39;
+            btnSalir.TabStop = false;
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "xmark-solid-full.png");
+            // 
+            // lblTituloForm
+            // 
+            lblTituloForm.Dock = DockStyle.Top;
+            lblTituloForm.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTituloForm.Location = new Point(0, 0);
+            lblTituloForm.Margin = new Padding(4, 0, 4, 0);
+            lblTituloForm.Name = "lblTituloForm";
+            lblTituloForm.Padding = new Padding(9, 0, 9, 0);
+            lblTituloForm.Size = new Size(891, 77);
+            lblTituloForm.TabIndex = 38;
+            lblTituloForm.Text = "Detalles de la compra";
+            lblTituloForm.TextAlign = ContentAlignment.MiddleLeft;
+            // 
             // VerDetallesCompraForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(713, 616);
+            CancelButton = btnCerrar;
+            ClientSize = new Size(891, 813);
+            Controls.Add(btnSalir);
+            Controls.Add(lblTituloForm);
             Controls.Add(btnCerrar);
             Controls.Add(btnEditar);
             Controls.Add(dgvDetalles);
@@ -128,13 +177,14 @@ namespace GestionVentasCel.views.compra
             Controls.Add(lblTotal);
             Controls.Add(lblFecha);
             Controls.Add(lblProveedor);
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            Margin = new Padding(3, 4, 3, 4);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 5, 4, 5);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "VerDetallesCompraForm";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Detalles de Compra";
+            Load += VerDetallesCompraForm_Load;
             ((System.ComponentModel.ISupportInitialize)dgvDetalles).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -149,5 +199,8 @@ namespace GestionVentasCel.views.compra
         private DataGridView dgvDetalles;
         private Button btnCerrar;
         private Button btnEditar;
+        private Button btnSalir;
+        private Label lblTituloForm;
+        private ImageList imageList1;
     }
 }
--- GestionVentasCel/views/compra/VerDetallesCompraForm.cs
+++ GestionVentasCel/views/compra/VerDetallesCompraForm.cs
@@ -3,8 +3,10 @@ using System.Globalization;
 using GestionVentasCel.controller.articulo;
 using GestionVentasCel.controller.compra;
 using GestionVentasCel.controller.proveedor;
+using GestionVentasCel.enumerations.modoForms;
 using GestionVentasCel.models.categoria;
 using GestionVentasCel.models.compra;
+using GestionVentasCel.temas;
 
 namespace GestionVentasCel.views.compra
 {
@@ -38,14 +40,12 @@ namespace GestionVentasCel.views.compra
 
         private void CargarDatos()
         {
-            this.Text = $"Detalles de Compra - {_compra.Fecha:dd/MM/yyyy}";
+            lblProveedor.Text += _compra.Proveedor.Nombre;
+            lblFecha.Text += _compra.Fecha.ToString("dd/MM/yyyy HH:mm");
+            lblTotal.Text += _compra.Total.ToString("C2", new CultureInfo("es-AR"));
+            lblObservaciones.Text += _compra.Observaciones ?? "Sin observaciones";
 
-            lblProveedor.Text = _compra.Proveedor?.Nombre ?? "N/A";
-            lblFecha.Text = _compra.Fecha.ToString("dd/MM/yyyy HH:mm");
-            lblTotal.Text = _compra.Total.ToString("C2", new CultureInfo("es-AR"));
-            lblObservaciones.Text = _compra.Observaciones ?? "Sin observaciones";
 
-            
             var _listaDetalle = _compra.Detalles.ToList();
 
             _detalleCompra = new BindingList<DetalleCompra>(_listaDetalle);
@@ -102,7 +102,7 @@ namespace GestionVentasCel.views.compra
 
         private void btnEditar_Click(object sender, EventArgs e)
         {
-            
+
             var compraCompleta = _compraController.GetByIdWithDetails(_compra.Id);
             if (compraCompleta != null)
             {
@@ -115,7 +115,64 @@ namespace GestionVentasCel.views.compra
                     CargarDatos();
                 }
             }
-            
+
+        }
+
+        private void ConfigurarEstilosVisuales()
+        {
+            this.BackColor = Tema.ColorSuperficie;
+
+
+            this.lblTituloForm.ForeColor = Tema.ColorTextoPrimario;
+            this.lblTituloForm.BackColor = Tema.ColorFondo;
+            this.btnSalir.BackColor = Tema.ColorFondo;
+
+
+            this.BackColor = Tema.ColorSuperficie;
+
+            // Cambiar los colores de los labels y el fondo de los inputs
+            this.lblProveedor.ForeColor = Tema.ColorFondo;
+            this.lblFecha.ForeColor = Tema.ColorFondo;
+            this.lblObservaciones.ForeColor = Tema.ColorFondo;
+            this.lblTotal.ForeColor = Tema.ColorFondo;
+
+            // Configuración del DGV. Esto se puede hacer en el diseñador, pero acá queda mas visible el código
+
+            // Eliminar divisores entre columnas y filas
+            dgvDetalles.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dgvDetalles.GridColor = dgvDetalles.BackgroundColor;
+
+            // Eliminar divisores entre columnas del header
+            dgvDetalles.AdvancedColumnHeadersBorderStyle.All = DataGridViewAdvancedCellBorderStyle.None;
+
+            // Cambiar el color de fondo, de la letra y el tamaño de fuente de la fila del header
+            dgvDetalles.EnableHeadersVisualStyles = false;
+            dgvDetalles.ColumnHeadersDefaultCellStyle.BackColor = Tema.ColorFondo;
+            dgvDetalles.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvDetalles.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+
+            // Colorear alternando las filas
+            dgvDetalles.RowsDefaultCellStyle.BackColor = Color.White;
+            dgvDetalles.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+
+            // Eliminar la columna de seleccion y configurar los modos de seleccion
+            dgvDetalles.RowHeadersVisible = false;
+            dgvDetalles.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvDetalles.MultiSelect = false;
+            dgvDetalles.ColumnHeadersDefaultCellStyle.SelectionBackColor = Tema.ColorFondo;
+
+
+
+        }
+
+        private void VerDetallesCompraForm_Load(object sender, EventArgs e)
+        {
+            this.ConfigurarEstilosVisuales();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.btnCerrar.PerformClick();
         }
     }
 }
