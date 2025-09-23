7171c629279fac72f892fd96cc38c54fa296e7fc
maxacan <maximilianoacan@gmail.com>
2025-09-23 17:54:33 -0300

    WIP modelos

---
+++ GestionVentasCel/models/ventas/DetalleVenta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestionVentasCel.models.articulo;
+
+namespace GestionVentasCel.models.ventas
+{
+    public class DetalleVenta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int VentaId { get; set; }
+        [Required]
+        public Venta Venta {  get; set; }
+
+        public int Cantidad {  get; set; }
+
+        // artículo o servicio
+        public int? ArticuloId { get; set; }
+        public Articulo? Articulo { get; set; }
+
+        public int? ServicioId { get; set; }
+        public Servicio? Servicio { get; set; }
+    }
+}
---
+++ GestionVentasCel/models/ventas/Venta.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GestionVentasCel.enumerations.ventas;
+using GestionVentasCel.models.clientes;
+using GestionVentasCel.models.usuario;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace GestionVentasCel.models.ventas
+{
+    public class Venta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, Display(Name ="Fecha de creación")]
+        // Fecha de creación de la venta/presupuesto
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        [Display(Name ="Fecha de venta")]
+        // Fecha de venta real. Si este valor es null, entonces
+        // se trata de un presupuesto y no de una venta.
+        // Al realizar la venta, se pobla la fecha y hora con la fecha y hora actual,
+        // convirtiendo el registro en una venta
+        public DateTime? FechaRealizacion { get; set; }
+
+        // El usuario que autorizó la venta
+        public int UsuarioId {  get; set; }
+        public Usuario? Usuario { get; set; }
+
+        // El cliente al que se le hizo la venta.
+        public int ClienteId {  get; set; }
+        public Cliente? Cliente { get; set; }
+
+        public TipoPagoEnum TipoPago { get; set; }
+
+
+    }
+}
